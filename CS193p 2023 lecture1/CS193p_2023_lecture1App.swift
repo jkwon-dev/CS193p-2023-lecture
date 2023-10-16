2f5b624f739a4b8ba316fb0a6aa3c66c6cd44aec
jkwon-dev <kwonejk@gmail.com>
2023-10-16 17:59:30 +0800

    Initial Commit

---
+++ CS193p 2023 lecture1/CS193p_2023_lecture1App.swift
@@ -0,0 +1,17 @@
+//
+//  CS193p_2023_lecture1App.swift
+//  CS193p 2023 lecture1
+//
+//  Created by kwon eunji on 2023/10/16.
+//
+
+import SwiftUI
+
+@main
+struct CS193p_2023_lecture1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS193p 2023 lecture1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CS193p 2023 lecture1
+//
+//  Created by kwon eunji on 2023/10/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
