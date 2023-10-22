dcbaff8f1d3f210a87b4c68555c2890ffeb37ab2
jkwon-dev <kwonejk@gmail.com>
2023-10-22 17:46:11 +0800

    LEcture 3 - create model, modelview

---
+++ CS193p 2023 lecture1/EmojiMemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiMemoryGame.swift
+//  CS193p 2023 lecture1
+//
+//  Created by kwon eunji on 10/22/23.
+//
+
+import Foundation
---
+++ CS193p 2023 lecture1/MemorizeGame.swift
@@ -0,0 +1,8 @@
+//
+//  MemorizeGame.swift
+//  CS193p 2023 lecture1
+//
+//  Created by kwon eunji on 10/22/23.
+//
+
+import Foundation
