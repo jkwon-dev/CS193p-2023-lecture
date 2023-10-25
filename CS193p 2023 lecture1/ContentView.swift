ef146b67c37d0ba8bb3addfcc530de46ea31a77b
jkwon-dev <kwonejk@gmail.com>
2023-10-25 10:14:32 +0800

    Lecture 3 - init model, modelview

--- CS193p 2023 lecture1/ContentView.swift
+++ CS193p 2023 lecture1/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    var viewModel: EmojiMemoryGame
+    
     let emojis = ["👻", "🎃", "🕷️", "😈", "💀", "🕸️", "🧙‍♀️", "🙀", "👹", "😱", "☠️", "🍭"]
     
     var body: some View {
--- CS193p 2023 lecture1/EmojiMemoryGame.swift
+++ CS193p 2023 lecture1/EmojiMemoryGame.swift
@@ -3,6 +3,24 @@
 //  CS193p 2023 lecture1
 //
 //  Created by kwon eunji on 10/22/23.
-//
+//  view model
 
-import Foundation
+import SwiftUI
+
+
+class EmojiMemoryGame {
+    static let emojis = ["👻", "🎃", "🕷️", "😈", "💀", "🕸️", "🧙‍♀️", "🙀", "👹", "😱", "☠️", "🍭"]
+    
+    static func createMemoryGane() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in  emojis[pairIndex]
+        }
+    }
+    
+    
+    private var model: MemoryGame<String> = createMemoryGane()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+}
